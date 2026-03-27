c7fc8c0f369a5b90cb832f543d47bb1fdf046bbd
mihai <mihairazvan03@yahoo.com>
2026-03-27 18:00:47 +0200

    Create project + Implement basic agent loop

---
+++ src/client.py
@@ -0,0 +1,26 @@
+from anthropic import Anthropic
+from anthropic.types import Message
+from anthropic.types.message_param import  MessageParam
+
+class AnthropicClient:
+    def __init__(
+            self,
+            model_name: str,
+            max_tokens: int = 1024
+    ) -> None:
+        self.model_name = model_name
+        self.max_tokens = max_tokens
+
+        self.client: Anthropic = Anthropic()
+        self.message_history: list[MessageParam] = []
+
+    def call(self, new_user_message: MessageParam) -> Message:
+        self.message_history.append(new_user_message)
+
+        new_assistant_message = self.client.messages.create(
+            model=self.model_name,
+            max_tokens=self.max_tokens,
+            messages=self.message_history
+        )
+
+        return new_assistant_message
---
+++ src/main.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import yaml
+from pathlib import Path
+from typing import Dict
+from client import AnthropicClient
+from agent_loop import loop
+
+
+PROJECT_DIR = Path(__file__).resolve().parent.parent
+
+"""Loads the config yaml"""
+def load_config(config_path: Path) -> Dict:
+
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    return config
+
+"""Body of the entry point"""
+def main() -> None:
+
+    load_dotenv()
+    config_path = PROJECT_DIR / "config.yaml"
+    config = load_config(config_path)
+
+    client = AnthropicClient(model_name=config["model_name"])
+    loop(client=client)
+
+if __name__ == "__main__":
+    main()
